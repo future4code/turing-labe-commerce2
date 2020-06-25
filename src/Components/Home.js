84d3806263f087231ac2337522ab1baaa5ff1dff
Lucas Alencar Miranda <lucasa.miranda@hotmail.com>
2020-06-25 10:36:26 -0300

    Inicio de Funcionalidades(Lucas-Mirand)OK

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -26,58 +26,83 @@ const ContainerProdutos = styled.div`
   margin-top: 8px;
 `
 
+const listProducts = [
+  {
+    id: 1,
+    title: 'Foguete Lucas',
+    price: 50,
+    url: 'https://picsum.photos/200/200?a=1',
+  },
+  {
+    id: 2,
+    title: 'Foguete Jailsom',
+    price: 60,
+    url: 'https://picsum.photos/200/200?a=2',
+  },
+  {
+    id: 3,
+    title: 'Foguete Diego',
+    price: 70,
+    url: 'https://picsum.photos/200/200?a=3',
+  },
+  {
+    id: 4,
+    title: 'Foguete Vinicius',
+    price: 80,
+    url: 'https://picsum.photos/200/200?a=4',
+  },
+  {
+    id: 5,
+    title: 'Foguete Miranda',
+    price: 40,
+    url: 'https://picsum.photos/200/200?a=5',
+  },
+  {
+    id: 6,
+    title: 'Foguete Nogueira',
+    price: 30,
+    url: 'https://picsum.photos/200/200?a=6',
+  },
+  {
+    id: 7,
+    title: 'Foguete Miyabara',
+    price: 20,
+    url: 'https://picsum.photos/200/200?a=7',
+  },
+  {
+    id: 8,
+    title: 'Foguete Fredeanelle',
+    price: 10,
+    url: 'https://picsum.photos/200/200?a=8',
+  },
+  
+]
+
 class Home extends React.Component {
+    state = {
+      products: listProducts
+    }
     render(){
+      console.log(this.state.products);
     return (
       <ContainerHome>
         <HomeHeader>
-            <span>Quantidade de Produtos: 8</span>
+    <span>Quantidade de Produtos: {this.state.products.length}</span>
             <select>
                 <option value="ordem crescente">Preço: Crescente</option>
                 <option value="ordem decrescente">Preço: Decrescente</option>
             </select>
         </HomeHeader>
         <ContainerProdutos>
-          <CardProduct 
-            url={"https://picsum.photos/200/200?a=1"}
-            title={"Foguete Lucas"}
-            price={150}
-          />
-          <CardProduct 
-            url={"https://picsum.photos/200/200?a=2"}
-            title={"Foguete Jailsom"}
-            price={150}
-          />
-          <CardProduct 
-            url={"https://picsum.photos/200/200?a=3"}
-            title={"Foguete Diego"}
-            price={150}
-          />
-          <CardProduct 
-            url={"https://picsum.photos/200/200?a=4"}
-            title={"Foguete Vinicius"}
-            price={150}
-          />
-          <CardProduct 
-            url={"https://picsum.photos/200/200?a=5"}
-            title={"Foguete Miranda"}
-            price={150}
-          />
-          <CardProduct 
-            url={"https://picsum.photos/200/200?a=6"}
-            title={"Foguete Nogueira"}
-            price={150}
-          />
-          <CardProduct 
-            url={"https://picsum.photos/200/200?a=7"}
-            title={"Foguete Miyabara"}
-            price={150}
-          />
-          <CardProduct 
-            url={"https://picsum.photos/200/200?a=8"}
-            title={"Foguete Fredeanelle"}
-            price={150}
-          />
+              {this.state.products.map((product, index) => {
+                return (
+                  <CardProduct 
+                    title={product.title}
+                    price={product.price}
+                    url={product.url}
+                  />
+                )
+              })}
         </ContainerProdutos>
       </ContainerHome>
     );
